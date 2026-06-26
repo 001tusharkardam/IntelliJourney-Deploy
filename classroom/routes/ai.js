cb098c7a367edccae15e1e4034baa80747d9dd31
001tusharkardam <165477443+001tusharkardam@users.noreply.github.com>
2026-06-26 14:43:13 +0530

    Update ai.js

--- classroom/routes/ai.js
+++ classroom/routes/ai.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const aiController = require("../../controllers/ai.js");
 const wrapAsync = require("../../utils/wrapAsync.js");
-
+const { isLoggedIn } = require("../../middleware.js");
 router.route("/plan")
-    .get(aiController.renderPlanForm)
-    .post(wrapAsync(aiController.generatePlan));
-
+    .get(isLoggedIn, aiController.renderPlanForm)
+    .post(isLoggedIn, wrapAsync(aiController.generatePlan));
 module.exports = router;
